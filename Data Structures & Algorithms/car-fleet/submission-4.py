59bda9f5039ac6be01e1f2b83801d3ee59a4455d
Hemant Sharma <109502318+hemanth-sharma@users.noreply.github.com>
2026-08-16 22:52:49 +0200

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,49 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # 1 2 3 4 5 6 7 8 9 10
+        # while true
+        # i += 3 = 4, 7, 10
+        # j += 2 = 6, 8, 10
+        # group the fleets 
+        # compute the total fleets
+
+        # y = x + time*v
+        # (y - x)/v = time -> (10-1)/2 => 9/2 = CEIL(4.5)
+        # find time for each car
+        # time: [] # store them in hashmap using time as keys? hmm
+        # target=12
+        # position=[10,8,0,5,3]
+        # speed=[2,4,1,1,3]
+        # 0 1 2 3 4 5 6 7 8 9 10 11 12
+        # i     i   i               ii
+        # 10, 8, 5, 3, 0
+        # 1, 1, 7, 3, 12 # # 1, 1, 12, 7, 3
+        # Hmm sort the cars position and speed then calculate time
+        
+        # cars = sorted(zip(position, speed), reverse=True)
+        # fleets = 0
+        # max_time = 0 # slowest time of the front car (cars are sorted by position so slowest so far is highest position one)
+        # for p, s in cars: 
+        #     time = (target-p)/s
+
+        #     if time > max_time:
+        #         max_time = time
+        #         fleets += 1
+            
+        # return fleets
+
+
+        # Stack method 
+        cars = sorted(zip(position, speed), reverse=True)
+        stack = []
+        for p, s in cars: 
+            time = (target-p)/s
+            stack.append(time)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        
+        return len(stack)
+
+
+
+
